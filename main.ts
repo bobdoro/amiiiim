9f145d05e53116e731504e598219de5626198bfb
bobdoro <166851857+bobdoro@users.noreply.github.com>
2024-04-12 14:00:59 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+    basic.pause(1000)
+})
